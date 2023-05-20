8cd9c32a396b64102c9c59db55a25025f23fa694
Adrian Bartnik <adrian.bartnik@netlight.com>
2023-05-20 21:28:37 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Mappy - Google Maps Navigation for Alfred
+
+This Alfred Workflow makes it easy to quickly access the Google Maps Navigation.
+It either works with a simple address search or by combining `from` & `to` based on your pre-defined home or work address.
+
+<video src='assets/demo.mp4' />
+
+## Usage
+
+You can use the `mappy` keyword to invoke this workflow from Alfred.
+It provides two main features: quick access based on your current location or based on pre-configured home/work addresses.
+In addition, a default transportation mode can be set or set per search.
+
+### Default Address Search based on your current location
+
+```shell
+mappy <your address query>
+```
+
+This opens the default Google Navigation with the address query as destination and your location as origin.
+
+### Navigation to / from your home / work address
+
+```shell
+mappy [to|from] [home|work] <your address query>
+```
+
+This opens the default Google Navigation with the home/work address and the address query as origin/destination respectively.
+The home and work address need to be configured, see below.
+
+### Transportation mode
+
+You can configure a default transportation mode (car, walking, bike, public transport) for each search.
+In addition, you can also configure the transportation per search by appending it to the commands above, e.g.:
+
+```shell
+mappy <your address query> car
+mappy to home <your address query> walking
+```
+
+A description of the different transportation modes can be found in the [Google Documentation](https://developers.google.com/maps/documentation/urls/get-started).
+The latest list of valid transportation modes can be looked up in [lines 24 of the `google-maps.py` file](google-maps.py).
+
+| Transportation Mode | Description                            | Alternative Keys                  |
+| ------------------- | -------------------------------------- | --------------------------------- |
+| driving             | Car transportation                     | `["car", "drive", "driving"]`     |
+| bicycling           | Bike travel                            | `["bike", "cycle", "bicycling"]`  |
+| transit             | Public Transport                       | `["public", "transit", "train"]`  |
+| walking             | Walking                                | `["walk", "walking"]`             |
+| default             | Automatically choosing the best option | `["default", "best", "adaptive"]` |
+
+
+## Configuration
+
+During installation and in the workflow configuration, you have the possibility to set:
+
+* Your home address
+* Your work address
+* Your default mode of transportation
+* The default Google domain
+
+## Development
+
+This project is built with Python 3.9 and Poetry.
+To get started, run `poetry install`.
+
+### Linting
+
+We use `flake8` and `black` to lint our code.
+Run the following to lint the code:
+
+```shell
+poetry run pflake8 .
+poetry run black .
+```
---
+++ google-maps.py
@@ -0,0 +1,144 @@
+import json
+import os
+import sys
+from enum import Enum
+import urllib.parse
+import re
+
+
+class TravelMode(Enum):
+    DRIVING = "driving"
+    WALKING = "walking"
+    BICYCLING = "bicycling"
+    TRANSIT = "transit"
+    DEFAULT = ""
+
+
+HOME_ADDRESS = os.environ.get("home_address")
+WORK_ADDRESS = os.environ.get("work_address")
+TRAVEL_MODE = TravelMode(os.environ.get("travel_mode"))
+LOCALISATION = os.environ.get("localisation")
+
+BASE_URL = f"https://www.google{LOCALISATION}/maps/dir/?api=1&"
+
+DRIVING_TRAVEL_MODES = ["car", "drive", "driving"]
+BIKING_TRAVEL_MODES = ["bike", "cycle", "bicycling"]
+TRANSIT_TRAVEL_MODES = ["public", "transit", "train"]
+WALKING_TRAVEL_MODES = ["walk", "walking"]
+DEFAULT_TRAVEL_MODES = ["default", "best", "adaptive"]
+ALL_TRAVEL_MODES = (
+    DRIVING_TRAVEL_MODES
+    + BIKING_TRAVEL_MODES
+    + TRANSIT_TRAVEL_MODES
+    + WALKING_TRAVEL_MODES
+    + DEFAULT_TRAVEL_MODES
+)
+
+
+def _parse_travel_mode(travel_mode_str: str) -> TravelMode:
+    if travel_mode_str in DRIVING_TRAVEL_MODES:
+        return TravelMode.DRIVING
+    if travel_mode_str in BIKING_TRAVEL_MODES:
+        return TravelMode.BICYCLING
+    if travel_mode_str in TRANSIT_TRAVEL_MODES:
+        return TravelMode.TRANSIT
+    if travel_mode_str in WALKING_TRAVEL_MODES:
+        return TravelMode.WALKING
+    if travel_mode_str in DEFAULT_TRAVEL_MODES:
+        return TravelMode.DEFAULT
+    else:
+        return TRAVEL_MODE
+
+
+def prepare_google_maps_url(
+    origin: str = "", destination: str = "", travel_mode: TravelMode = TRAVEL_MODE
+):
+    return (
+        BASE_URL + f"origin={urllib.parse.quote(origin)}"
+        f"&destination={urllib.parse.quote(destination)}"
+        f"&travelmode={urllib.parse.quote(travel_mode.value)}"
+    )
+
+
+def prepare_from_query(query: str):
+    pattern = f"^(from|to) (work|home) (.+?)(?: +({'|'.join(DRIVING_TRAVEL_MODES)}|{'|'.join(BIKING_TRAVEL_MODES)}|p{'|'.join(TRANSIT_TRAVEL_MODES)}|{'|'.join(WALKING_TRAVEL_MODES)}))?$"
+
+    match = re.search(pattern, query)
+
+    if match:
+        operation = match.group(1)
+        work_or_home = match.group(2)
+        destination = match.group(3)
+        travel_mode = _parse_travel_mode(match.group(4))
+
+        origin = HOME_ADDRESS if work_or_home == "home" else WORK_ADDRESS
+
+        if not origin:
+            return f"ERROR: Your {'home' if work_or_home == 'home' else 'work'} address is not set. You can do this in the Workflow Configuration."
+
+        if operation == "to":
+            origin, destination = destination, origin
+
+        return prepare_google_maps_url(
+            origin=origin, destination=destination, travel_mode=travel_mode
+        )
+    else:
+        return query
+
+
+def prepare_from_default_query(query: str) -> str:
+    query_split = query.rsplit(" ", 1)
+
+    if len(query_split) == 2 and query_split[1] in ALL_TRAVEL_MODES:
+        travel_mode = _parse_travel_mode(query_split[1])
+
+        return prepare_google_maps_url(
+            destination=query_split[0], travel_mode=travel_mode
+        )
+    else:
+        return prepare_google_maps_url(destination=query)
+
+
+if __name__ == "__main__":
+    search_query = sys.argv[1].lower()
+
+    print(
+        "Starting Mappy for:", search_query, WORK_ADDRESS, HOME_ADDRESS, file=sys.stderr
+    )
+
+    items = []
+
+    if not search_query.startswith("from ") and not search_query.startswith("to "):
+        items.append(
+            {
+                "title": "<address>",
+                "subtitle": "Navigate to address based on current location. Optional: append <travelmode>",
+                "arg": prepare_from_default_query(search_query),
+                "icon": {"path": "assets/icon.png"},
+            }
+        )
+
+    if (
+        search_query.startswith(tuple(["f", "fr", "fro", "from"]))
+        or len(search_query) == 0
+    ):
+        items.append(
+            {
+                "title": "From <home|work> <address>",
+                "subtitle": "Navigate to <address> from home or work address. Optional: append <travelmode>",
+                "arg": prepare_from_query(query=search_query),
+                "icon": {"path": "assets/icon.png"},
+            }
+        )
+
+    if search_query.startswith(tuple(["t", "to"])) or len(search_query) == 0:
+        items.append(
+            {
+                "title": "To <home|work> <address>",
+                "subtitle": "Navigate home or work to <address>. Optional: append <travelmode>",
+                "arg": prepare_from_query(query=search_query),
+                "icon": {"path": "assets/icon.png"},
+            }
+        )
+
+    sys.stdout.write(json.dumps({"items": items}))
